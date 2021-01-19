4ac08046280ee7dcde76da8194be8e7b06294cbf
Théo Guidoux <appcombusiness@gmail.com>
2021-01-19 09:49:33 +0100

    poneys

--- poneys/src/Poneys.php
+++ poneys/src/Poneys.php
@@ -25,8 +25,24 @@ class Poneys
      */
     public function removePoneyFromField(int $number): void
     {
-        $this->count -= $number;
+				if($number > $this->count){
+					throw new Exception("Y'en a trop Narvalo", 100);
+				}
+				$this->count -= $number;
+
     }
+
+		/**
+     * Ajoute un poney dans champ
+     *
+     * @param int $number Nombre de poneys à ajouter
+     *
+     * @return void
+     */
+		 public function addPoneyToField(int $number): void
+		 {
+		 		$this->count += $number;
+		 }
 
     /**
      * Retourne les noms des poneys
@@ -37,5 +53,15 @@ class Poneys
     {
 
     }
+
+		/**
+		*Retourne TRUE si s'il reste de l'espace libre dans le champ
+		*
+		* @return boolean
+		*/
+		public function hasSlotLeft(): boolean
+		{
+			return $this->getCount() < 15;
+		}
 }
 ?>
--- poneys/tests/PoneysTest.php
+++ poneys/tests/PoneysTest.php
@@ -4,25 +4,77 @@ use PHPUnit\Framework\TestCase;
 require_once 'src/Poneys.php';
 
 /**
- * Classe de test de gestion de poneys
- */
+* Classe de test de gestion de poneys
+*/
 class PoneysTest extends TestCase
 {
-    /**
-     * Undocumented function
-     *
-     * @return void
-     */
-    public function testRemovePoneyFromField()
-    {
-        // Setup
-        $poneys = new Poneys();
+	/**
+	* Undocumented function
+	*
+	* @return void
+	*/
+	public function testRemovePoneyFromField()
+	{
+		// Setup
+		$poneys = new Poneys();
 
-        // Action
-        $poneys->removePoneyFromField(3);
+		// Action
+		$poneys->removePoneyFromField(3);
 
-        // Assert
-        $this->assertEquals(5, $poneys->getCount());
-    }
+		// Assert
+		$this->assertEquals(5, $poneys->getCount());
+
+		$this->assertEquals($this->expectExceptionCode(100), $poneys->removePoneyFromField(10));
+	}
+
+	/**
+	* Undocumented function
+	*
+	* @return void
+	*/
+	public function testAddPoneyToField()
+	{
+		// Setup
+		$poneys = new Poneys();
+
+		// Action
+		$poneys->addPoneyToField("3");
+
+		// Assert
+		$this->assertEquals(11, $poneys->getCount());
+	}
+
+	/**
+	* @dataProvider removeProvider
+	*/
+	public function testRemove(int $number, int $expected): void
+	{
+
+		$poneys = new Poneys();
+
+		$poneys->removePoneyFromField($number);
+		$this->assertEquals($expected, $poneys->getCount());
+	}
+
+	public function removeProvider(): array
+	{
+
+		return [
+			[0, 8],
+			[1, 7]
+		];
+	}
+
+	/**
+	* insert sth here
+	*/
+	public function testNames(): void
+	{
+		$poneys = $this->createMock('Poneys');
+		$poneys->method('getNames')->willReturn(["abc", "xyz"]);
+
+		$this->assertEquals(["abc", "xyz"], $poneys->getNames());
+	}
 }
+
 ?>
